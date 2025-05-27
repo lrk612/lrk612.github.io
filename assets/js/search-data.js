4683f6621f60c99ff24fedfea2d3ef0063b174d1
lrk612 <131778006+lrk612@users.noreply.github.com>
2025-05-27 11:22:05 +0000

    Deploying to gh-pages from @ lrk612/lrk612.github.io@cc9f5b3ea7e2d5e574e5d6c9da50a2410d87cc2d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -461,18 +461,25 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
+        id: 'social-dblp',
+        title: 'DBLP',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://dblp.org/pid/224/4672-2", "_blank");
+        },
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6C%69%72%6B%36%31%32@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/lrk612", "_blank");
         },
       },{
         id: 'social-rss',
@@ -486,14 +493,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=Ruikun Li", "_blank");
         },
       },{
       id: 'light-theme',
